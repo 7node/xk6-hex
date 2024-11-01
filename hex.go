562ddfb41572aebcd1f5285c732d426302e52ff6
gh1st <lxb429@outlook.com>
2024-11-01 09:51:44 +0800

    init commit

---
+++ hex.go
@@ -0,0 +1,25 @@
+package hex
+
+import (
+	"go.k6.io/k6/js/modules"
+
+	goHex "encoding/hex"
+)
+
+// init is called by the Go runtime at application startup.
+func init() {
+	modules.Register("k6/x/hex", new(Hex))
+}
+
+// Hex is the type for our custom API.
+type Hex struct {
+}
+
+// Decode returns the decoded string.
+func (c *Hex) Decode(hex string) []byte {
+	decoded, err := goHex.DecodeString(hex)
+	if err != nil {
+		return nil
+	}
+	return decoded
+}
